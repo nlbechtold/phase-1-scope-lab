0f3e9992d7ef000c1e1f6c556f011edf12a608eb
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-10 09:56:22 -0600

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+
+// function myFunc() {
+//     const myVar = 42;
+  
+//     return myVar * 2;
+//   }
+//   // => undefined
+  
+//   myFunc();
+
+function upperCaseCustomerName() {
+    
+    customerName = "BOB"
+  
+
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "whatever"
+   leastFavoriteCustomer = "not whatever"
+    
+}
+
+const leastFavoriteCustomer = "not whatever"
